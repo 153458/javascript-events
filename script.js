bf6953b5abf1005d3d575be3acfb60eee7475c8c
berra <berratekin03@gmail.com>
2025-04-01 18:32:01 -0400

    Added keyboard events

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const mouseButton = document.getElementById("mouseButton");
 const mouseMessage = document.getElementById("mouseMessage");
 mouseButton.addEventListener("mouseover", () => mouseMessage.textContent = "Mouse is over the button!");
 mouseButton.addEventListener("mouseout", () => mouseMessage.textContent = "Mouse left the button!");
+
+const keyInput = document.getElementById("keyInput");
+const keyMessage = document.getElementById("keyMessage");
+keyInput.addEventListener("keyup", (event) => keyMessage.textContent = event.key);
