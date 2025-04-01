0fe862e554da8946476f57d25db0adae7bcd90d0
berra <berratekin03@gmail.com>
2025-04-01 18:33:15 -0400

    Added form events

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ mouseButton.addEventListener("mouseout", () => mouseMessage.textContent = "Mouse
 const keyInput = document.getElementById("keyInput");
 const keyMessage = document.getElementById("keyMessage");
 keyInput.addEventListener("keyup", (event) => keyMessage.textContent = event.key);
+
+const eventForm = document.getElementById("eventForm");
+const formMessage = document.getElementById("formMessage");
+eventForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    formMessage.textContent = "Form submitted successfully!";
+});
