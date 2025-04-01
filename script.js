b5953370e2a93f59ff34f5e4cc8a88defb04114b
berra <berratekin03@gmail.com>
2025-04-01 18:30:49 -0400

    Added mouse events in javascript

---
+++ script.js
@@ -0,0 +1,4 @@
+const mouseButton = document.getElementById("mouseButton");
+const mouseMessage = document.getElementById("mouseMessage");
+mouseButton.addEventListener("mouseover", () => mouseMessage.textContent = "Mouse is over the button!");
+mouseButton.addEventListener("mouseout", () => mouseMessage.textContent = "Mouse left the button!");
